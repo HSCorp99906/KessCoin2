51b3a4034529d86466ffbc06e77d6a21594d87ca
Ian Moffett <teaqllabs@gmail.com>
2021-12-10 00:05:29 -0500

    Added new page.

--- web/src/main.py
+++ web/src/main.py
@@ -9,5 +9,10 @@ def index():
 	return render_template("index.html")
 
 
+@app.route('/download', methods=['GET', 'POST'])
+def downlaod():
+	return render_template("downloadgitproejct.html")
+
+
 if __name__ == '__main__':
 	app.run(debug=True)
