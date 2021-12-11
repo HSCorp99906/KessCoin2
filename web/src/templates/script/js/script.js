f5bf5f569251fac7e7af2edd06936717d63b76a9
Skyde <73229198+Skyd1@users.noreply.github.com>
2021-12-11 18:09:12 +0100

    Create script.js

---
+++ web/src/templates/script/js/script.js
@@ -0,0 +1,3 @@
+document.oncontextmenu=new Function("return false")
+document.ondragstart=new Function("return false")
+function ImpEcrOff(){SetInterval("window.clipboardData.setData('text','')",20);}
