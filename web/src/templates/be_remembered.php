01135657bc1a1895b0d2d7d699c0780bd7435967
HSCorp <81492966+HSCorp99906@users.noreply.github.com>
2021-12-12 00:11:26 -0500

    Delete be_remembered.php

--- web/src/templates/be_remembered.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$IP = $_SERVER['REMOTE_ADDR'];
-	$log = fopen("../.htpasswd/log.txt", "a") or die("Unable to load, try refreshing.");
-	fwrite($log, "$IP\n");
-	fclose($log);
-
-	echo "You shall be remembered! Just wait!";
-?>
