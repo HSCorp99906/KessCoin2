0fcdf6e6c08c75b59fa370747cf17aee693042f4
Ian Moffett <teaqllabs@gmail.com>
2021-12-18 21:34:37 -0500

    Improved Wallet.

--- web/src/templates/wallet.php
+++ web/src/templates/wallet.php
@@ -1,7 +1,9 @@
 <html>
 	<head>
 		<title>Wallet</title>
+		<meta name="description" content="KessCoin Wallet">
 	
+
 		<style>
 			#pk {
 				filter: blur(4px);
@@ -11,6 +13,10 @@
 
 			#pk:hover {
 				filter: blur(0px);
+			}
+
+			#transactions {
+				border: 5px solid black;				
 			}
 		</style>
 
@@ -47,12 +53,12 @@
 
 		?>
 
-		
+
 		<h1>Wallet</h1>
 
 		<div class="wallet-info">
 			<p>Private Key</p>
-			<input id="pk" value=<?php echo $_SESSION['privkey'] ?> readonly>	
+			<input id="pk" value=<?php echo $_SESSION['privkey'] ?> readonly>
 			<h4>Your public key is: <?php echo $_SESSION['pubkey']?></h4>
 			<h4>Your balance is: <?php echo file_get_contents("../secrets/$pubkey/balance.bal", true);?></h4>
 		</div>
@@ -80,20 +86,27 @@
             $lenfa = strlen($_SESSION["pubkey"]);
 			$amt = $_POST["amt"];
             $error = false;
+			$to_addr = $_POST["ta"];
+			$from_addr = $_POST["fa"];
+
+			$okay = $from_addr == $_SESSION["pubkey"];
         ?>
 
-        <?php if ($lenta != 34 && $lenta > 0 || $lenta > 34) {
+<?php if ($lenta != 34 && $lenta > 0 || $lenfa != 34 && $lenta > 0 || !(file_exists("../secrets/$to_addr")) && strlen($to_addr) > 0 || !($okay) && $from_addr > 0 || $to_addr > 0 && $to_addr == $from_addr) {
             $error = true;
             ?>
-            <h1 style="background-color: rgb(255, 0, 0)">INVALID TO ADDRESS/AMOUNT.</h1>
+            <h1 style="background-color: rgb(255, 0, 0)">INVALID TO-ADDR/FROM-ADDR/AMOUNT</h1>
         <?php
-            } elseif (!($error) && $lenta == 34 && $_COOKIE['confirm'] == "true" && !(isset($_COOKIE['visited']))) {
+			} elseif (!($error) && $lenta == 34 && $_COOKIE['confirm'] == "true" && !(isset($_COOKIE['visited']))) {
+				srand(date());
                 $transaction = fopen("newtransaction", "a");
                 $tinfo = $_POST['ta'];
                 $tinfo .= PHP_EOL;
                 $tinfo .= $_SESSION["pubkey"];
                 $tinfo .= PHP_EOL;
-                $tinfo .= $_POST['amt'];
+				$tinfo .= $_POST['amt'];
+				$tinfo .= PHP_EOL;
+				$tinfo .= rand() + rand();
 
 				$fa = $_POST['fa'];
 
@@ -102,7 +115,6 @@
                 sleep(20);
                 unlink("newtransaction");
 				$cur_id = rand();
-				$rn1 = rename("../secrets/sha1.sign", "../secrets/sha1-$cur_id-$fa.sign");
 				$rn2 = rename("../secrets/newtransaction", "../secrets/transaction-$cur_id-$fa");
 
 				if (rn1 == false || rn2 == false) {
@@ -115,8 +127,39 @@
 				setcookie("confirm", "true", time() - 3600);
 				header("Location: https://kesscoin.com/");
 			}
-        ?>
+
+			$addr = $_SESSION["pubkey"];
+
+			$files = glob("../secrets/*$addr");
+			$transactionData = array();
+			foreach($files as $file) {
+				$handle = fopen($file, "r");
+				$content = null;
+
+				print_r($file);
+
+				while (!(feof($handle))) {
+					$content = fgets($handle);
+					array_push($transactionData, $content);
+				}
+
+				fclose($handle);
+			}
+	?>
+
+		<br><br>
+
+		<h1>Pending Transactions</h1>
+		<div id="transactions">
+
+			<?php
+				$thingidx = 0;  // 1: TO_ADDRESS, 2: FROM_ADDRESS, 3: AMOUNT
+			?>
+		
+			<h1><?php foreach($transactionData as $t) { if ($thingidx == 1) { echo "TO_ADDRESS: $t<br>"; } else if ($thingidx == 2) { echo "FROM ADDRESS: $t<br>"; } else if ($thingidx == 3) { echo "AMOUNT: $t KC<br>";} else if ($thingidx == 4) { echo "ID: $t<br>"; } $thingidx++;continue; }?></h1>
+		</div>	
 
 		<br><br><a href="/logout.php">Logout</a>
+
 	</body>
 </html>
